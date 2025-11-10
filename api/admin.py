69b272625dc06099d51bc4602d6b14cba4c8a8a4
IbragimKhaniev <newinterny@gmail.com>
2025-11-10 16:10:22 +0300

    Add Member model and register in admin panel

--- api/admin.py
+++ api/admin.py
@@ -1,3 +1,23 @@
 from django.contrib import admin
+from .models import Member
 
-# Register your models here.
+
+@admin.register(Member)
+class MemberAdmin(admin.ModelAdmin):
+    list_display = ('email', 'first_name', 'last_name', 'created_at')
+    list_filter = ('created_at',)
+    search_fields = ('email', 'first_name', 'last_name')
+    readonly_fields = ('created_at',)
+    ordering = ('-created_at',)
+
+    fieldsets = (
+        ('Personal Information', {
+            'fields': ('email', 'first_name', 'last_name')
+        }),
+        ('Security', {
+            'fields': ('password',)
+        }),
+        ('Timestamps', {
+            'fields': ('created_at',)
+        }),
+    )
--- api/models.py
+++ api/models.py
@@ -1,3 +1,24 @@
 from django.db import models
+from django.contrib.auth.hashers import make_password
 
-# Create your models here.
+
+class Member(models.Model):
+    email = models.EmailField(unique=True, verbose_name='Email')
+    first_name = models.CharField(max_length=150, verbose_name='First Name')
+    last_name = models.CharField(max_length=150, verbose_name='Last Name')
+    password = models.CharField(max_length=128, verbose_name='Password')
+    created_at = models.DateTimeField(auto_now_add=True, verbose_name='Created At')
+
+    class Meta:
+        verbose_name = 'Member'
+        verbose_name_plural = 'Members'
+        ordering = ['-created_at']
+
+    def __str__(self):
+        return f"{self.first_name} {self.last_name} ({self.email})"
+
+    def save(self, *args, **kwargs):
+        if self.pk is None or 'password' in kwargs.get('update_fields', []):
+            if not self.password.startswith('pbkdf2_sha256$'):
+                self.password = make_password(self.password)
+        super().save(*args, **kwargs)
